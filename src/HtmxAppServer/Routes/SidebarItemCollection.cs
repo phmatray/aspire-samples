ec50bd4470a30618344da15efb55ceb8e63068ca
Philippe Matray <phmatray@gmail.com>
2024-03-07 10:20:07 +0100

    sequoia menu item

--- src/HtmxAppServer/Routes/SidebarItemCollection.cs
+++ src/HtmxAppServer/Routes/SidebarItemCollection.cs
@@ -26,31 +26,59 @@ public class SidebarItemCollection : List<SidebarItem>
 {
     public SidebarItemCollection()
     {
-        AddSidebarLink(RouteDashboard, "Dashboard", "Squares2X2");
-        AddSidebarLink(RouteLeads, "Leads", "InboxArrowDown");
-        AddSidebarLink(RouteTransactions, "Transactions", "CurrencyDollar");
-        AddSidebarLink(RouteCharts, "Analytics", "ChartBar");
-        AddSidebarLink(RouteIntegration, "Integration", "Bolt");
-        AddSidebarLink(RouteCalendar, "Calendar", "CalendarDays");
+        // AddSidebarLink(RouteDashboard, "Dashboard", "Squares2X2");
+        // AddSidebarLink(RouteLeads, "Leads", "InboxArrowDown");
+        // AddSidebarLink(RouteTransactions, "Transactions", "CurrencyDollar");
+        // AddSidebarLink(RouteCharts, "Analytics", "ChartBar");
+        // AddSidebarLink(RouteIntegration, "Integration", "Bolt");
+        // AddSidebarLink(RouteCalendar, "Calendar", "CalendarDays");
         
-        AddSidebarSubmenu("Settings", "Cog6Tooth", [
-            new SidebarLink(RouteProfileSettings, "Profile", "User"),
-            new SidebarLink(RouteBills, "Billing", "Wallet"),
-            new SidebarLink(RouteTeam, "Team Members", "Users")
+        // AddSidebarSubmenu("Settings", "Cog6Tooth", [
+        //     new SidebarLink(RouteProfileSettings, "Profile", "User"),
+        //     new SidebarLink(RouteBills, "Billing", "Wallet"),
+        //     new SidebarLink(RouteTeam, "Team Members", "Users")
+        // ]);
+        //
+        // AddSidebarSubmenu("Pages", "DocumentDuplicate", [
+        //     new SidebarLink(RouteLogin, "Login", "ArrowRightOnRectangle"),
+        //     new SidebarLink(RouteRegister, "Register", "User"),
+        //     new SidebarLink(RouteForgotPassword, "Forgot Password", "Key"),
+        //     new SidebarLink(RouteBlank, "Blank Page", "Document"),
+        //     new SidebarLink(RouteCode404, "404", "ExclamationTriangle")
+        // ]);
+        //
+        // AddSidebarSubmenu("Documentation", "DocumentText", [
+        //     new SidebarLink(RouteGettingStarted, "Getting Started", "DocumentText"),
+        //     new SidebarLink(RouteDocFeatures, "Features", "TableCells"),
+        //     new SidebarLink(RouteDocComponents, "Components", "CodeBracketSquare")
+        // ]);
+        
+        AddSidebarSubmenu("Aperçu", "Eye", [
+            new SidebarLink(RouteGettingStarted, "Missions", "ClipboardDocumentCheck"),
+            new SidebarLink(RouteGettingStarted, "Documents", "DocumentText"),
+        ]);
+        
+        AddSidebarSubmenu("Profil", "Person", [
+            new SidebarLink(RouteGettingStarted, "Adresses", "MapMarker"),
+            new SidebarLink(RouteGettingStarted, "Nationalité", "Flag"),
+            new SidebarLink(RouteGettingStarted, "Numéros d'identification", "Identification"),
+            new SidebarLink(RouteGettingStarted, "Relations légales", "UserGroup"),
+            new SidebarLink(RouteGettingStarted, "Famille et filiation", "Users"),
+            new SidebarLink(RouteGettingStarted, "Relations BCE", "BuildingOffice"),
+            new SidebarLink(RouteGettingStarted, "Logging Mutations", "ClipboardDocumentList"),
         ]);
 
-        AddSidebarSubmenu("Pages", "DocumentDuplicate", [
-            new SidebarLink(RouteLogin, "Login", "ArrowRightOnRectangle"),
-            new SidebarLink(RouteRegister, "Register", "User"),
-            new SidebarLink(RouteForgotPassword, "Forgot Password", "Key"),
-            new SidebarLink(RouteBlank, "Blank Page", "Document"),
-            new SidebarLink(RouteCode404, "404", "ExclamationTriangle")
-        ]);
-        
-        AddSidebarSubmenu("Documentation", "DocumentText", [
-            new SidebarLink(RouteGettingStarted, "Getting Started", "DocumentText"),
-            new SidebarLink(RouteDocFeatures, "Features", "TableCells"),
-            new SidebarLink(RouteDocComponents, "Components", "CodeBracketSquare")
+        AddSidebarSubmenu("Carrière", "Briefcase", [
+            new SidebarLink(RouteGettingStarted, "Historique des affiliations", "History"),
+            new SidebarLink(RouteGettingStarted, "Demandes d'aide", "Lifebuoy"),
+            new SidebarLink(RouteGettingStarted, "Carrière en gestion", "Briefcase"),
+            new SidebarLink(RouteGettingStarted, "Aidants et aidés", "HandThumbUp"),
+            new SidebarLink(RouteGettingStarted, "Aperçu de cotisations", "CurrencyEuro"),
+            new SidebarLink(RouteGettingStarted, "E-Clipz", "Film"),
+            new SidebarLink(RouteGettingStarted, "Transfert EU", "GlobeEuropeAfrica"),
+            new SidebarLink(RouteGettingStarted, "Revenus", "CurrencyEuro"),
+            new SidebarLink(RouteGettingStarted, "Carrière Moteur Pension", "Car"),
+            new SidebarLink(RouteGettingStarted, "Attestation de carrière", "DocumentCheck"),
         ]);
     }
 
